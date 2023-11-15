98b1c25e2952a7b634f92a52ee046b7eeb6f0281
lgoldin <leandrogoldin5@gmail.com>
2023-11-15 13:32:30 -0300

    Refactor

--- Kata/Model/RomanNumeral.cs
+++ Kata/Model/RomanNumeral.cs
@@ -4,14 +4,7 @@
     {
         public string Convert(int value)
         {
-            if(value == 1)
-            {
-                return "I";
-            }
-            else
-            {
-                return "V";
-            }
+            return value == 1 ? "I" : "V";
             
         }
     }
