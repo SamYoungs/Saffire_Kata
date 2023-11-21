8c892647dac3363f07f5729f0e784d861933414d
lgoldin <leandrogoldin5@gmail.com>
2023-11-21 18:07:26 -0300

    We've completed the romannumerals KATA

--- Kata/Model/RomanNumeral.cs
+++ Kata/Model/RomanNumeral.cs
@@ -2,14 +2,42 @@
 {
     public class RomanNumeral
     {
+        private readonly Dictionary<int, string> romanNumeralsDictionary = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
         public string Convert(int value)
         {
-            if (value == 1) return "I";
-            if (value == 2) return "II";
-            if (value == 3) return "III";
+            if (value < 1 || value > 3000)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
 
-            return "V";
-            
+            string romanNumerals = string.Empty;
+
+            foreach(var (key, romanNumeral) in romanNumeralsDictionary)
+            {
+                while (value >= key)
+                {
+                    romanNumerals += romanNumeral;
+                    value -= key;
+                }
+            }
+
+            return romanNumerals;
         }
     }
 }
--- Kata/Test/RomanNumeralsTests.cs
+++ Kata/Test/RomanNumeralsTests.cs
@@ -4,44 +4,71 @@ namespace Test
 {
     public class RomanNumeralsTests
     {
-        [Test]
-        public void GivenA1ReturnAnI()
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        [TestCase(24, "XXIV")]
+        [TestCase(25, "XXV")]
+        [TestCase(29, "XXIX")]
+        [TestCase(30, "XXX")]
+        [TestCase(34, "XXXIV")]
+        [TestCase(35, "XXXV")]
+        [TestCase(39, "XXXIX")]
+        [TestCase(40, "XL")]
+        [TestCase(50, "L")]
+        [TestCase(54, "LIV")]
+        [TestCase(55, "LV")]
+        [TestCase(59, "LIX")]
+        [TestCase(90, "XC")]
+        [TestCase(94, "XCIV")]
+        [TestCase(95, "XCV")]
+        [TestCase(99, "XCIX")]
+        [TestCase(100, "C")]
+        [TestCase(123, "CXXIII")]
+        [TestCase(295, "CCXCV")]
+        [TestCase(399, "CCCXCIX")]
+        [TestCase(400, "CD")]
+        [TestCase(464, "CDLXIV")]
+        [TestCase(478, "CDLXXVIII")]
+        [TestCase(500, "D")]
+        [TestCase(755, "DCCLV")]
+        [TestCase(900, "CM")]
+        [TestCase(949, "CMXLIX")]
+        [TestCase(1000, "M")]
+        [TestCase(1111, "MCXI")]
+        [TestCase(2377, "MMCCCLXXVII")]
+        [TestCase(2992, "MMCMXCII")]
+        [TestCase(3000, "MMM")]
+        public void GivenAnIntReturnARomanNumeral(int value, string expectedResult)
         {
             var romanNumeral = new RomanNumeral();
 
-            string result = romanNumeral.Convert(1);
+            string result = romanNumeral.Convert(value);
 
-            Assert.IsTrue("I" == result);
+            Assert.IsTrue(result == expectedResult);
         }
 
         [Test]
-        public void Given2ReturnAnII()
+        public void GivenAZeroExpectedAnExpection()
         {
-            var romanNumeral = new RomanNumeral();
-
-            string result = romanNumeral.Convert(2);
-
-            Assert.IsTrue("II" == result);
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new RomanNumeral().Convert(0));
         }
 
         [Test]
-        public void Given3ReturnAnIII()
+        public void GivenANumberGreaterThan3000ExpectedAnExpection()
         {
-            var romanNumeral = new RomanNumeral();
-
-            string result = romanNumeral.Convert(3);
-
-            Assert.IsTrue("III" == result);
-        }
-
-        [Test]
-        public void GivenA5ReturnAnV()
-        {
-            var romanNumeral = new RomanNumeral();
-
-            string result = romanNumeral.Convert(5);
-
-            Assert.IsTrue("V" == result);
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => new RomanNumeral().Convert(3001));
         }
     }
 }
